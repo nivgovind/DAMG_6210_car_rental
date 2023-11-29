0b8fdead00ceb808315b114e53cdd4aa5ff5e5fe
ssaurabh760 <144853458+ssaurabh760@users.noreply.github.com>
2023-11-29 17:59:59 -0500

    Fixing 3_Vendor.sql

--- 3_Vendor.sql
+++ 3_Vendor.sql
@@ -3,51 +3,61 @@ CREATE OR REPLACE PROCEDURE add_vendor_vehicle (
     p_model VARCHAR2,
     p_transmission_type VARCHAR2,
     p_category VARCHAR2,
-    p_fuel_type VARCHAR2
+    p_fuel_type VARCHAR2,
+    p_hourly_rate NUMBER,
+    p_miles_driven NUMBER,
+    p_passenger_capacity NUMBER,
+    p_registration_id VARCHAR2,
+    p_current_location_name VARCHAR2
 ) AS
+    v_vendor_id NUMBER;
+    v_vehicle_type_id NUMBER;
+    v_location_id NUMBER;
 BEGIN
-    -- Check for non-null values for required parameters
-    IF p_make IS NOT NULL AND p_model IS NOT NULL AND p_category IS NOT NULL AND p_fuel_type IS NOT NULL THEN
-        INSERT INTO vehicle_types (make, model, transmission_type, category, fuel_type)
-        VALUES (p_make, p_model, p_transmission_type, p_category, p_fuel_type);
-        COMMIT;
-    ELSE
-        -- Raise an exception if any of the required parameters is null
-        RAISE_APPLICATION_ERROR(-20001, 'Required parameters cannot be null');
+
+    -- Check if the vehicle type already exists
+    SELECT id INTO v_vehicle_type_id
+    FROM vehicle_types
+    WHERE make = p_make AND model = p_model AND ROWNUM = 1;
+
+    IF v_vehicle_type_id IS NULL THEN
+        -- Raise an exception if the vehicle type does not exist
+        RAISE_APPLICATION_ERROR(-20003, 'Specified vehicle type does not exist');
     END IF;
+
+    -- Check if the location already exists
+    SELECT id INTO v_location_id
+    FROM locations
+    WHERE name = p_current_location_name AND ROWNUM = 1;
+
+    IF v_location_id IS NULL THEN
+        -- Raise an exception if the location does not exist
+        RAISE_APPLICATION_ERROR(-20004, 'Specified location does not exist');
+    END IF;
+
+    -- Insert the new vehicle
+    INSERT INTO vehicles (
+        hourly_rate,
+        miles_driven,
+        availability_status,
+        passenger_capacity,
+        registration_id,
+        current_location_id,
+        users_id,
+        vehicle_type_id
+    ) VALUES (
+        p_hourly_rate,
+        p_miles_driven,
+        1,
+        p_passenger_capacity,
+        p_registration_id,
+        v_location_id,
+        v_vendor_id,
+        v_vehicle_type_id
+    );
+
+    COMMIT;
 END add_vendor_vehicle;
-/
-
-CREATE OR REPLACE PROCEDURE update_vehicle (
-    p_vehicle_types_id NUMBER,
-    p_make VARCHAR2,
-    p_model VARCHAR2,
-    p_transmission_type VARCHAR2,
-    p_category VARCHAR2,
-    p_fuel_type VARCHAR2
-) AS
-BEGIN
-    -- Check if the specified vehicle ID exists
-    IF NOT EXISTS (SELECT 1 FROM vehicle_types WHERE id = p_vehicle_types_id) THEN
-        -- Raise an exception if the vehicle ID does not exist
-        RAISE_APPLICATION_ERROR(-20002, 'Specified vehicle ID does not exist');
-    END IF;
-
-    -- Check for non-null values for required parameters
-    IF p_make IS NOT NULL AND p_model IS NOT NULL AND p_category IS NOT NULL AND p_fuel_type IS NOT NULL THEN
-        UPDATE vehicle_types
-        SET make = p_make,
-            model = p_model,
-            transmission_type = p_transmission_type,
-            category = p_category,
-            fuel_type = p_fuel_type
-        WHERE id = p_vehicle_types_id;
-        COMMIT;
-    ELSE
-        -- Raise an exception if any of the required parameters is null
-        RAISE_APPLICATION_ERROR(-20001, 'Required parameters cannot be null');
-    END IF;
-END update_vehicle;
 /
 
 
