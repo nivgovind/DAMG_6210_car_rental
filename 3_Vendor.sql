d22011b590490f1e0148a869b4a501fe6d1d2973
ssaurabh760 <144853458+ssaurabh760@users.noreply.github.com>
2023-11-29 14:45:11 -0500

    Update 3_Vendor.sql

--- 3_Vendor.sql
+++ 3_Vendor.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURE add_vehicle (
+CREATE OR REPLACE PROCEDURE add_vendor_vehicle (
     p_make VARCHAR2,
     p_model VARCHAR2,
     p_transmission_type VARCHAR2,
@@ -8,18 +8,18 @@ CREATE OR REPLACE PROCEDURE add_vehicle (
 BEGIN
     -- Check for non-null values for required parameters
     IF p_make IS NOT NULL AND p_model IS NOT NULL AND p_category IS NOT NULL AND p_fuel_type IS NOT NULL THEN
-        INSERT INTO vehicles (make, model, transmission_type, category, fuel_type)
+        INSERT INTO vehicle_types (make, model, transmission_type, category, fuel_type)
         VALUES (p_make, p_model, p_transmission_type, p_category, p_fuel_type);
         COMMIT;
     ELSE
         -- Raise an exception if any of the required parameters is null
         RAISE_APPLICATION_ERROR(-20001, 'Required parameters cannot be null');
     END IF;
-END add_vehicle;
+END add_vendor_vehicle;
 /
 
 CREATE OR REPLACE PROCEDURE update_vehicle (
-    p_vehicle_id NUMBER,
+    p_vehicle_types_id NUMBER,
     p_make VARCHAR2,
     p_model VARCHAR2,
     p_transmission_type VARCHAR2,
@@ -28,20 +28,20 @@ CREATE OR REPLACE PROCEDURE update_vehicle (
 ) AS
 BEGIN
     -- Check if the specified vehicle ID exists
-    IF NOT EXISTS (SELECT 1 FROM vehicle_types WHERE id = p_vehicle_id) THEN
+    IF NOT EXISTS (SELECT 1 FROM vehicle_types WHERE id = p_vehicle_types_id) THEN
         -- Raise an exception if the vehicle ID does not exist
         RAISE_APPLICATION_ERROR(-20002, 'Specified vehicle ID does not exist');
     END IF;
 
     -- Check for non-null values for required parameters
     IF p_make IS NOT NULL AND p_model IS NOT NULL AND p_category IS NOT NULL AND p_fuel_type IS NOT NULL THEN
-        UPDATE vehicles
+        UPDATE vehicle_types
         SET make = p_make,
             model = p_model,
             transmission_type = p_transmission_type,
             category = p_category,
             fuel_type = p_fuel_type
-        WHERE id = p_vehicle_id;
+        WHERE id = p_vehicle_types_id;
         COMMIT;
     ELSE
         -- Raise an exception if any of the required parameters is null
@@ -49,6 +49,7 @@ BEGIN
     END IF;
 END update_vehicle;
 /
+
 
 
     
