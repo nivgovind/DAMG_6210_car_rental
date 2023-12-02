3b03c4eed80e57491de6940ea3be35fe00c6f15e
nivcodes <govindaraj.n@northeastern.edu>
2023-12-01 20:02:36 -0500

    error fixes

--- 3_Vendor.sql
+++ 3_Vendor.sql
@@ -2,7 +2,7 @@ alter session set current_schema=cr_app_admin;
 set serveroutput on;
 
 -- Add a car to the database
-EXEC add_vehicle(30.00, 5000, 'true', 5, 'MIN123NE0W456OOP', 'New York', 'Bob', 'silverado')
+EXEC add_vehicle(30.00, 5000, 'true', 5, 'MIN123NE0W456OOP', 'New York', 'Bob', 'silverado');
 
 -- Update the rate of a car
 EXEC update_car_availability('BOS123NE0W456OOP', 'false');
